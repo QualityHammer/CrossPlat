1da8a62ab7d5cba7803d1578008fb1c564fe776a
QualityHammer <agingllama@gmail.com>
2020-04-06 21:57:37 -0400

    Added an event loop because the window wouldn't show up on macosx with only a delay.

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,16 @@
 
 int main() {
 	Client::Window window{};
-	SDL_Delay(3000);
+  SDL_Event e;
+  bool quit{false};
+  while (!quit) {
+    while (SDL_PollEvent(&e)) {
+      switch (e.type) {
+        case SDL_QUIT:
+          quit = true;
+          break;
+      }
+    }
+  }
 	return 0;
 }
