7f93cd269988fa3cb117d7f01e7abfd87b8d4f29
QualityHammer <agingllama@gmail.com>
2020-04-08 00:41:57 -0400

    Raytracer now displays textures on the walls.

--- src/client/render/Render.cpp
+++ src/client/render/Render.cpp
@@ -24,6 +24,73 @@ void renderRect(Pixels& pixels, const u16 x,
     }
 }
 
+Pixels textureColumn(const Texture& texture, const size_t texID,
+                   const u16 coordX, const u16 columnHeight) {
+    const size_t imgW{texture.size * texture.count};
+    const size_t imgH{texture.size};
+    assert(texture.pixels.size() == imgW * imgH &&
+           coordX < texture.size && texID < texture.count);
+    Pixels column{};
+    column.resize(columnHeight);
+    for (size_t y{0}; y < columnHeight; y++) {
+        size_t px{texID * texture.size + coordX};
+        size_t py{(y * texture.size) / columnHeight};
+        column[y] = texture.pixels[px + py * imgW];
+    }
+    return column;
+}
+
+void renderColumn(Pixels& pixels, const MapData& gMap,
+                  const TextureManager& tMan,
+                  const Player& player, const float& cx,
+                  const float& cy, const float& angle,
+                  const float& dist, const u16 rayNum,
+                  int& px, int& py, const int& mapIndex) {
+    size_t texID{static_cast<size_t>(gMap[mapIndex] - '0')};
+    const Texture& texture{tMan[TextureName::WALL]};
+    u16 columnHeight{
+        static_cast<u16>(WINDOW_HEIGHT /
+                         (dist * std::cos(angle - player.a)))};
+    float hitX{cx - std::floor(cx + 0.5f)};
+    float hitY{cy - std::floor(cy + 0.5f)};
+    int coordX{static_cast<int>(hitX * texture.size)};
+    // For vertical walls
+    if (std::abs(hitY) > std::abs(hitX)) {
+        coordX = hitY * texture.size;
+    }
+    if (coordX < 0) coordX += texture.size;
+    assert(coordX >= 0 && coordX < (int)texture.size);
+    
+    std::vector<Color> column =
+    textureColumn(texture, texID, coordX, columnHeight);
+    px = WINDOW_WIDTH + rayNum;
+    for (size_t j{0}; j < columnHeight; j++) {
+        py = j + WINDOW_HEIGHT / 2 - columnHeight / 2;
+        if (py < 0 || py >= (int)WINDOW_HEIGHT) continue;
+        pixels[px + py * WINDOW_WIDTH] = column[j];
+    }
+}
+
+void followRay(Pixels& pixels, const float& angle, const Player& player,
+               const MapData& gMap, const TextureManager& tMan,
+               const u16& rayNum) {
+    for (float t{0.0}; t < 20.0; t += 0.05) {
+        float cx{player.x + t * std::cos(angle)};
+        float cy{player.y + t * std::sin(angle)};
+        int px{static_cast<int>(cx * PIXEL_WIDTH)};
+        int py{static_cast<int>(cy * PIXEL_HEIGHT)};
+        const int mapIndex{(int)cx + (int)cy * MAP_WIDTH};
+        if (gMap[mapIndex] != ' ') {
+            renderColumn(pixels, gMap, tMan,
+                         player, cx, cy, angle,
+                         t, rayNum, px, py,
+                         mapIndex);
+            break;
+            
+        }
+    }
+}
+
 void render(const GameState& gameState, Pixels& pixels,
             const TextureManager& tMan) {
     const Player& player{gameState.player};
@@ -34,24 +101,8 @@ void render(const GameState& gameState, Pixels& pixels,
         float angle{player.a - FOV / 2 + FOV * i /
             (float)WINDOW_WIDTH
         };
-        for (float t{0.0}; t < 20.0; t += 0.05) {
-            float cx{player.x + t * std::cos(angle)};
-            float cy{player.y + t * std::sin(angle)};
-            int index{(int)cx + (int)cy * MAP_WIDTH};
-            if (gMap[index] != ' ') {
-                size_t texID{static_cast<size_t>(gMap[index] - '0')};
-                const Texture& texture{tMan[TextureName::WALL]};
-                u16 columnHeight{
-                    static_cast<u16>(WINDOW_HEIGHT /
-                                     (t * std::cos(angle - player.a)))};
-                renderRect(pixels, i,
-                           WINDOW_HEIGHT / 2 - columnHeight,
-                           1, columnHeight * 2,
-                           texture.getSingleColor(texID));
-                break;
-                
-            }
-        }
+        followRay(pixels, angle, player, gMap,
+                  tMan, i);
     }
 }
 
--- src/client/render/Render.h
+++ src/client/render/Render.h
@@ -11,7 +11,16 @@ void renderRect(Pixels& pixels, const u16 x,
                 const u16 y, const u16 w,
                 const u16 h, const Color color);
 
+void followRay(Pixels& pixels, const float& angle, const Player& player,
+                const MapData& gMap, const TextureManager& tMan,
+                const u16& rayNum);
+
 void render(const GameState& gameState, Pixels& pixels,
             const TextureManager& tMan);
+
+Pixels textureColumn(const Pixels& pixels, const size_t texSize,
+                   const size_t texNum, const size_t texID,
+                   const TextureName name, const u16 coordX,
+                   const u16 columnHeight);
 
 }
