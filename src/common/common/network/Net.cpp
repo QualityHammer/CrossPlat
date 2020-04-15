fb9c088f9a0d87bbe87001826b9b5fca97bb4f5a
QualityHammer <agingllama@gmail.com>
2020-04-15 18:29:02 -0400

    Cleaned up cmake files and switched in the full version of ENet.

---
+++ src/common/common/network/Net.cpp
@@ -0,0 +1,22 @@
+#include "Net.hpp"
+
+#include <iostream>
+
+#include <enet/enet.h>
+
+void netInit() {
+#ifdef WIN32
+  if (enet_initialize() != 0) {
+        std::cerr << "An error occurred when initializing ENet." << std::endl;
+        exit(1);
+    }
+#endif
+    return;
+}
+
+void win32Close() {
+#ifdef WIN32
+    atexit(enet_deinitialize);
+#endif
+    return;
+}
