bbc4737f9984cc60992185fdd1d07718f372f9dc
QualityHammer <agingllama@gmail.com>
2020-04-15 19:17:09 -0400

    Added some basic debug functions.

--- src/common/common/Debug.cpp
+++ src/common/common/Debug.cpp
@@ -1,1 +1,35 @@
 #include "Debug.hpp"
+
+#include <iostream>
+
+namespace Debug {
+
+void serverConnected(const std::string& address) {
+    std::cout << "Connection to " << address << " has succeeded." << std::endl;
+}
+
+void serverDisconnected() {
+    std::cout << "Disconnect successful" << std::endl;
+}
+
+void clientConnected() {
+    std::cout << "Client connected" << std::endl;
+}
+
+void clientDisconnected() {
+    std::cout << "Client disconnected" << std::endl;
+}
+
+void clientPacketRecieved() {
+    std::cout << "Packet recieved from client" << std::endl;
+}
+
+void serverPacketRecieved() {
+    std::cout << "Packet recieved from server" << std::endl;
+}
+
+void serverRunning(const std::string& address, const u16& port) {
+    std::cout << "Server running on " << address << ":" << port << std::endl;
+}
+
+}
--- src/common/common/Debug.hpp
+++ src/common/common/Debug.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <string>
+
+#include "constructs/Types.hpp"
+
+namespace Debug {
+
+// Client debug
+void serverConnected(const std::string& address);
+void serverDisconnected();
+void serverPacketRecieved();
+
+// Server debug
+void clientConnected();
+void clientDisconnected();
+void clientPacketRecieved();
+void serverRunning(const std::string& address, const u16& port);
+
+}
