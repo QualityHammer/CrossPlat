e67374245f52ef7e4ee7169acaac8713b861c474
QualityHammer <agingllama@gmail.com>
2020-04-17 16:03:46 -0400

    Simplified data structure of GameMap.

--- src/common/common/constructs/GameMap.cpp
+++ src/common/common/constructs/GameMap.cpp
@@ -2,12 +2,8 @@
 
 namespace Common {
 
-GameMap::GameMap(const GameMapData& data)
-: w{data.w}, h{data.h}, size{data.size},
-m_data{data.data} {}
-
-const u8& GameMap::operator[](const size_t index) const {
-    return m_data[index];
+const u8& GameMap::operator[](const u16 index) const {
+    return data[index];
 }
 
 }
--- src/common/common/constructs/GameMap.hpp
+++ src/common/common/constructs/GameMap.hpp
@@ -6,24 +6,32 @@
 
 namespace Common {
 
-struct GameMapData {
-    std::vector<u8> data;
+//struct GameMapData {
+//    std::vector<u8> data;
+//    u16 w;
+//    u16 h;
+//    size_t size;
+//};
+//
+//class GameMap {
+//public:
+//    GameMap(const GameMapData& data);
+//
+//    const u16 w;
+//    const u16 h;
+//    const size_t size;
+//
+//    const u8& operator[](const size_t index) const;
+//private:
+//    const std::vector<u8> m_data;
+//};
+struct GameMap {
     u16 w;
     u16 h;
-    size_t size;
-};
-
-class GameMap {
-public:
-    GameMap(const GameMapData& data);
+    u16 size;
+    std::vector<u8> data;
     
-    const u16 w;
-    const u16 h;
-    const size_t size;
-    
-    const u8& operator[](const size_t index) const;
-private:
-    const std::vector<u8> m_data;
+    const u8& operator[](const u16 index) const;
 };
 
 }
