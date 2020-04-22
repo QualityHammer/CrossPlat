f15eb3fa5ff38475ea33d36f4aa66d514e3d6135
QualityHammer <agingllama@gmail.com>
2020-04-21 23:12:29 -0400

    Moved float deserialization to a function.

--- src/common/common/network/Deserialize.cpp
+++ src/common/common/network/Deserialize.cpp
@@ -1,14 +1,24 @@
 #include "Deserialize.hpp"
+namespace {
+
+float deserializeFloat(const std::vector<u8>& data, const u16 offset) {
+    const u32 tmp{static_cast<u32>(data[offset] | (data[offset + 1] << 8) | (data[offset + 2] << 16) | (data[offset + 3] << 24))};
+    return *reinterpret_cast<const float*>(&tmp);
+}
+
+u16 deserializeU16(const std::vector<u8>& data, const u16 offset) {
+    return data[offset] | (data[offset + 1] << 8);
+}
+
+}
 
 namespace Net {
 
 Common::Entity deserializeEntity(const std::vector<u8>& data) {
-    const u32 tmpX{static_cast<u32>(data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24))};
-    const u32 tmpY{static_cast<u32>(data[4] | (data[5] << 8) | (data[6] << 16) | (data[7] << 24))};
     Common::Entity entity;
-    entity.x = *reinterpret_cast<const float*>(&tmpX);
-    entity.y = *reinterpret_cast<const float*>(&tmpY);
-    entity.texID = data[8];
+    entity.x = deserializeFloat(data, 0);
+    entity.y = deserializeFloat(data, sizeof(float));
+    entity.texID = data[sizeof(float) * 2];
     return entity;
 }
 
@@ -16,8 +26,8 @@ Common::GameMap deserializeGameMap(const std::vector<u8>& data) {
     Common::GameMap gMap;
     gMap.w = data[0];
     gMap.h = data[1];
-    gMap.size = data[2] | (data[3] << 8);
-    gMap.data.insert(gMap.data.end(), data.begin() + 4, data.end());
+    gMap.size = deserializeU16(data, 2);
+    gMap.data.insert(gMap.data.end(), data.begin() + sizeof(u16) + 2, data.end());
     return gMap;
 }
 
@@ -27,9 +37,8 @@ Common::Player deserializePlayer(const std::vector<u8>& data) {
     player.x = tmp.x;
     player.y = tmp.y;
     player.texID = tmp.texID;
-    const u32 tmpA{static_cast<u32>(data[9] | (data[10] << 8) | (data[11] << 16) | (data[12] << 24))};
-    player.a = *reinterpret_cast<const float*>(&tmpA);
-    player.PID = data[13];
+    player.a = deserializeFloat(data, sizeof(float) * 2 + 1);
+    player.PID = data[sizeof(float) * 3 + 1];
     return player;
 }
 
@@ -37,8 +46,7 @@ Common::PlayerControl deserializePlayerControl(const std::vector<u8>& data) {
     Common::PlayerControl playerControl;
     playerControl.moveX = data[0];
     playerControl.moveY = data[1];
-    const u32 tmpTurn{static_cast<u32>(data[2] | (data[3] << 8) | (data[4] << 16) | (data[5] << 24))};
-    playerControl.turn = *reinterpret_cast<const float*>(&tmpTurn);
+    playerControl.turn = deserializeFloat(data, 2);
     return playerControl;
 }
 
